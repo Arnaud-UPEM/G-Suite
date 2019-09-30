770a759549c73946e4264247469ee40540d85ae2
Arnaud <a.angely@upem.mq>
2019-09-30 16:55:13 -0400

    Update CodeGround.js

--- Employee/CodeGround.js
+++ Employee/CodeGround.js
@@ -1,3 +1,29 @@
-function Test_REPARTITION_SJO() {
-    REPARTITION_SJO.from_repartition();
+function Test_1() {
+    var Toto = function () {
+        this.toto = 0;
+    };
+
+    var Totos = function () {
+
+        this.totos = [];   // List of Employee
+
+
+        this.get = function (index) {
+            return this.totos[index];
+        };
+    };
+
+    var totos = new Totos();
+    totos.totos.push(new Toto());
+    totos.totos.push(new Toto());
+    totos.totos.push(new Toto());
+    totos.totos.push(new Toto());
+    totos.totos.push(new Toto());
+
+    Logger.log('%s', totos.totos);
+
+    var t = totos.get(2);
+    t.toto = 5;
+
+    Logger.log('%s', totos.totos);
 }
